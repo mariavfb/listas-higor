aee8746ed17e8689b37d3f5ef4ac1794dd502ae5
Wesley Willian <wesleywilliancg@gmail.com>
2023-05-28 20:45:51 -0300

    Add files via upload

---
+++ comunicacao-desafio/src/components/UsuarioDetalhe.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="usuario-detalhe">
+    <h2 v-if="!usuario">Usuário não selecionado!</h2>
+    <div v-else>
+      <h2>ID: {{ usuario.id }}</h2>
+      <h2>Nome: {{ usuario.nome }}</h2>
+      <h2>Idade: {{ usuario.idade }}</h2>
+      <button class="botao-remover" @click="removerSelecao">Remover Seleção</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { usuario: Object },
+  methods: {
+    removerSelecao() {
+      this.$emit('remover-selecao');
+    }
+  }
+}
+</script>
+
+<style>
+.usuario-detalhe {
+  flex: 1;
+  border: 1px solid #CCC;
+  padding: 20px;
+}
+
+.botao-remover {
+  background-color: #ff5757;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.botao-remover:hover {
+  background-color: #ff3b3b;
+}
+</style>
