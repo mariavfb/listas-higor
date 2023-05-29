281494ace2b7ebb5e03d81900cebfe5785760c75
Wesley Willian <wesleywilliancg@gmail.com>
2023-05-28 21:11:14 -0300

    Add files via upload

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Menu></Menu>
+    <AppBar></AppBar>
+
+    <v-content>
+      <Carousel></Carousel>
+    </v-content>
+
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+import AppBar from './components/AppBar.vue'
+import Carousel from './components/Carousel.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Menu,
+    AppBar,
+    Carousel,
+    Footer
+  }
+}
+</script>
